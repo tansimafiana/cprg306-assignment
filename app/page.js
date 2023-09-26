a0f7b83be7f868c7274a06f7fb98a719fdfa9321
tansimafiana <tansima.fiana@edu.sait.ca>
2023-09-25 23:04:37 -0600

    Add week2

--- app/page.js
+++ app/page.js
@@ -9,6 +9,8 @@ const HomePage = () => {
   return (
     <div>
       <h1>CPRG 306: Web Development 2 - Assignments</h1>
+      <p>Tansima Kamal Fiana</p>
+      
       <studentInfo />
       <Link href="/week2">week2</Link>
     </div>
