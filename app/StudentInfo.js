85f6e1f93a88e2ac2367d71831d6b2880ccc0f15
tansimafiana <tansima.fiana@edu.sait.ca>
2023-09-25 22:32:39 -0600

    Modify app Add StudentInfo

---
+++ app/StudentInfo.js
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+export default function studentInfo(){
+  return(
+    <div>
+      <h2>Name:Tansima Kamal Fiana</h2>
+      <h2>Course Section: CPRG 306 A</h2>
+      <Link href="https://github.com/tansimafiana/cprg306-assignments">Github Repository</Link>
+    </div>
+  );
+     
+  
+}
