c147a8e3e12ae754c988b6213beb9a1ad5667f6f
tansimafiana <tansima.fiana@edu.sait.ca>
2023-09-25 22:42:55 -0600

    Add week2

---
+++ app/week2/page.js
@@ -0,0 +1,14 @@
+import studentInfo from './StudentInfo' 
+export default function Home() {
+    return (
+     <main>
+      <h1> 
+        CPRG 306: Web Development 2 - Assignments
+      </h1>
+      <studentInfo/ >
+      <Link href="/week2">week 2</Link>
+  
+      
+      </main>
+    )
+  }
