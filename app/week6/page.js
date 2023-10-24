b5a253e2b62154339a1f31ce9d577cd49d25a0b5
tansimafiana <tansima.fiana@edu.sait.ca>
2023-10-24 11:05:11 -0600

    Add week6

--- app/week6/page.js
+++ app/week6/page.js
@@ -1,2 +1,15 @@
 import React from 'react';
 import studentInfo from './StudentInfo' 
+const Week6Page = () => {
+    return (
+      <div>
+        <h1>My Shopping List</h1>
+        <studentInfo />
+        {/* Add your content for Week 2 here */}
+      </div>
+    );
+  };
+  
+  
+  
+  export default Week6Page;
